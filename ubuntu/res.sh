13708c848407dbd4f599ee708bcebcbd3e1c5c4c
Sergi Sorribas <ssorribas@gmail.com>
2014-07-14 12:53:27 +0200

    added vim snippets and res

---
+++ ubuntu/res.sh
@@ -0,0 +1,7 @@
+#/bin/bash
+
+xrandr --newmode "1920x1200_60.00"  193.16  1920 2048 2256 2592  1200 1201 1204 1242  -HSync +Vsync
+xrandr --addmode VBOX1 1920x1080_60.00
+xrandr --addmode VBOX0 1920x1080_60.00
+xrandr --output VBOX0 --mode 1920x1080_60.00
+xrandr --output VBOX1 --mode 1920x1080_60.00 --left-of VBOX0
